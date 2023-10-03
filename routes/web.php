cf2c8a4478ea87f8c2d2d3edd22679adddaf9720
samadee123 <samadeekularatne78@gmail.com>
2023-10-04 01:15:47 +0530

    AddDeleteUpdateProduct

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,8 +26,25 @@ Route::middleware([
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
-    })->name('dashboard');
+    })->name('dashboard'); 
 });
 
 route::get('/redirect',[HomeController::class,'redirect']);
 
+route::get('/view_catagory',[AdminController::class,'view_catagory']);
+
+route::post('/add_catagory',[AdminController::class,'add_catagory']);
+
+route::get('/delete_catagory/{id}',[AdminController::class,'delete_catagory']);
+
+route::get('/view_product',[AdminController::class,'view_product']);
+
+route::post('/add_product',[AdminController::class,'add_product']);
+
+route::get('/show_product',[AdminController::class,'show_product']);
+
+route::get('/delete_product/{id}',[AdminController::class,'delete_product']);
+
+route::get('/update_product/{id}',[AdminController::class,'update_product']);
+
+route::post('/update_product_confirm/{id}',[AdminController::class,'update_product_confirm']);
